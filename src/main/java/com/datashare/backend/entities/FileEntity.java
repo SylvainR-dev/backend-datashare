4bf6f735159e29a149746bd522739c64d9568caa
Sylvain-Rudelle <rudellesylvain@gmail.com>
2026-05-27 10:49:31 +0200

    Brique 4 = Ajout de sésurité avec env et gitignore

--- src/main/java/com/datashare/backend/entities/FileEntity.java
+++ src/main/java/com/datashare/backend/entities/FileEntity.java
@@ -1,7 +1,11 @@
+
+// Pour toute explication détaillée, voir d'abord le fichier UserEntity et comparer. 
+
 package com.datashare.backend.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +20,9 @@ import java.time.LocalDateTime;
 @Table(name = "files")
 public class FileEntity {
 
+// Ici ce sont les tables que l'on retrouvera dans la base de donnée. 
+// Attention : ce n'est pas la base de données qui définit les tables, mais Entity. 
+// Parce que grâce au fichier "application.yaml" (dans ressources), on peut configurer Spring Boot pour qu'il crée automatiquement les tables en base de données à partir de ces entités !!!!!
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +33,7 @@ public class FileEntity {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @NotBlank
+    @NotNull
     @Column(name= "size", nullable = false)
     private Long size;
 
